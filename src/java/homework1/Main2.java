dc1c52a60af841881ebab9944754fc8db855e412
Адмін <koliapopkov3@gmail.com>
2023-10-10 19:15:28 +0300

    Commit

--- src/java/homework1/Main2.java
+++ src/java/homework1/Main2.java
@@ -1,20 +1,29 @@
-package homework1;
-
-import java.util.Random;
-
+import java.util.Scanner;
 public class Main2 {
     public static void main(String[] args) {
-
-        Random random = new Random();
-
-        int num1 = -20;
-        int num2 = 20;
-
-        int count = 1;
-
-        for (int i = 0; i < count; i++) {
-            int randomNumber = random.nextInt(num2 - num1 + 1) + num1;
-            System.out.println(randomNumber);
+        double num1;
+        double num2;
+        double ans;
+        char op;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        num1 = reader.nextDouble();
+        num2 = reader.nextDouble();
+        System.out.print("\nEnter an operator (+, -, *, /): ");
+        op = reader.next().charAt(0);
+        switch(op) {
+            case '+': ans = num1 + num2;
+                break;
+            case '-': ans = num1 - num2;
+                break;
+            case '*': ans = num1 * num2;
+                break;
+            case '/': ans = num1 / num2;
+                break;
+            default:  System.out.printf("Error! Enter correct operator");
+                return;
         }
+        System.out.print("\nThe result is given as follows:\n");
+        System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
     }
 }
