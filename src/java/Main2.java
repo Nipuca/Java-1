35f7df838062c15322e56407ce469cfa4a196166
Адмін <koliapopkov3@gmail.com>
2023-10-12 16:35:02 +0300

    Commit

--- src/java/Main2.java
+++ src/java/Main2.java
@@ -1,31 +1,53 @@
-package Main2.java;
-
 import java.util.Scanner;
+
 public class Main2 {
     public static void main(String[] args) {
-        double num1;
-        double num2;
-        double ans;
-        char op;
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter two numbers: ");
-        num1 = reader.nextDouble();
-        num2 = reader.nextDouble();
-        System.out.print("\nEnter an operator (+, -, *, /): ");
-        op = reader.next().charAt(0);
-        switch(op) {
-            case '+': ans = num1 + num2;
+        Scanner scanner = new Scanner(System.in);
+        double result = 0;
+
+        while (true) {
+            System.out.print("Введіть вираз: ");
+            String in = scanner.nextLine();
+
+            if (in.equals("q")) {
+                System.out.println("Вихід з калькулятора");
                 break;
-            case '-': ans = num1 - num2;
-                break;
-            case '*': ans = num1 * num2;
-                break;
-            case '/': ans = num1 / num2;
-                break;
-            default:  System.out.printf("Error! Enter correct operator");
-                return;
+            }
+
+            try {
+                result = evaluateExpression(in);
+                System.out.println("Результат: " + result);
+            } catch (Exception e) {
+                System.out.println("Помилка: " + e.getMessage());
+            }
         }
-        System.out.print("\nThe result is given as follows:\n");
-        System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+    }
+
+    public static double evaluateExpression(String expression) throws Exception {
+        String[] num = expression.split(" ");
+        if (num.length != 3) {
+            throw new Exception("Неправильний формат виразу.");
+        }
+
+        double operand1 = Double.parseDouble(num[0]);
+        double operand2 = Double.parseDouble(num[2]);
+        String operator = num[1];
+
+        switch (operator) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                if (operand2 != 0) {
+                    return operand1 / operand2;
+                } else {
+                    throw new Exception("ылення на 0 неможливо.");
+                }
+            default:
+                throw new Exception("Невідома операція.");
+        }
     }
 }
