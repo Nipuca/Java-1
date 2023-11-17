660e05e5df3136ac66956c9e789f46cc535a4580
Адмін <koliapopkov3@gmail.com>
2023-11-17 19:24:28 +0200

    Commit2

---
+++ src/java/homework2/Main.java
@@ -0,0 +1,13 @@
+package homework2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+        Car car2 = new Car(2020);
+        Car car3 = new Car(2018, 150.0);
+        Car car4 = new Car(2015, 120.0, 2000);
+        Car car5 = new Car(2012, 100.0, 1800, "Blue");
+    }
+}
+
